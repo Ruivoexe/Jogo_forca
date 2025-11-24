6434e5ab4e53484acab522a90b77057a7a180783
Vinicius Ferreira <41nicius.aleg@gmail.com>
2025-11-24 20:17:49 -0300

    pequenos ajustes 3

--- app/src/main/java/com/example/forca/MainActivity3.java
+++ app/src/main/java/com/example/forca/MainActivity3.java
@@ -1,8 +1,11 @@
 package com.example.forca;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +13,63 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.forca.MainActivity;
-import com.example.forca.R;
-
 public class MainActivity3 extends AppCompatActivity {
+
+    Button btnSair, btnA, btnB, btnC;
+    ImageView corpo;
+    int pontos=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
 
-        Button btnSair = findViewById(R.id.btnSair);
+        setContentView(R.layout.activity_main3);
+        pontos = getIntent().getIntExtra("pontosAtuais",0);
+
+        btnSair = findViewById(R.id.btnSair);
         btnSair.setOnClickListener(v -> {
-            Intent intent = new Intent(com.example.forca.MainActivity3.this, MainActivity.class);
+            Intent intent = new Intent(MainActivity3.this,MainActivity.class);
+
             startActivity(intent);
             finish();
         });
 
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        corpo = findViewById(R.id.corpo);
+
+        btnA.setOnClickListener(v -> respostaErrada(btnA));
+        btnB.setOnClickListener(v -> respostaCerta(btnB));
+        btnC.setOnClickListener(v -> respostaErrada(btnC));
+
     }
+
+    private void respostaCerta(Button botao)
+    {
+        botao.setBackgroundColor(Color.GREEN);
+        pontos++;
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity3.this,MainActivity4.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+    private void respostaErrada(Button botao) {
+        botao.setBackgroundColor(Color.RED);
+        corpo.setColorFilter(Color.RED);
+
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+
 }
