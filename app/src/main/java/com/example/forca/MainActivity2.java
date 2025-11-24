78433232d61849c9dccd9a1b254f3df4cdd8bf8a
Vinicius Ferreira <41nicius.aleg@gmail.com>
2025-11-24 19:27:29 -0300

    Lógica de perguntas e respostas

--- app/src/main/java/com/example/forca/MainActivity2.java
+++ app/src/main/java/com/example/forca/MainActivity2.java
@@ -1,8 +1,11 @@
 package com.example.forca;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +15,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btnSair, btnA, btnB, btnC;
+    ImageView cabeca;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button btnSair = findViewById(R.id.btnSair);
+        btnSair = findViewById(R.id.btnSair);
         btnSair.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity2.this,MainActivity.class);
             startActivity(intent);
             finish();
         });
 
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        cabeca = findViewById(R.id.cabeca);
+
+        btnA.setOnClickListener(v -> respostaErrada(btnA));
+        btnB.setOnClickListener(v -> respostaCerta(btnB));
+        btnC.setOnClickListener(v -> respostaErrada(btnC));
+
     }
+
+    private void respostaCerta(Button botao)
+    {
+        botao.setBackgroundColor(Color.GREEN);
+
+        new Handler().postDelayed(()->{
+            botao.setBackgroundTintList(null);
+            Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+    private void respostaErrada(Button botao) {
+        botao.setBackgroundColor(Color.RED);
+        cabeca.setColorFilter(Color.RED);
+
+        new Handler().postDelayed(()->{
+            botao.setBackgroundTintList(null);
+            Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+
 }
