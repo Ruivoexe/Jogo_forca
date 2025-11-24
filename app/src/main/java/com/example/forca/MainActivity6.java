c11f1750e29e1a03a7a17bb37d368452cdfbc1ee
Vinicius Ferreira <41nicius.aleg@gmail.com>
2025-11-24 20:58:59 -0300

    pequenos ajustes 5

--- app/src/main/java/com/example/forca/MainActivity6.java
+++ app/src/main/java/com/example/forca/MainActivity6.java
@@ -1,8 +1,11 @@
 package com.example.forca;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +13,63 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.forca.MainActivity;
-import com.example.forca.R;
-
 public class MainActivity6 extends AppCompatActivity {
+
+    Button btnSair, btnA, btnB, btnC;
+    ImageView perna_esq;
+    int pontos=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
 
-        Button btnSair = findViewById(R.id.btnSair);
+        setContentView(R.layout.activity_main6);
+        pontos = getIntent().getIntExtra("pontosAtuais",0);
+
+        btnSair = findViewById(R.id.btnSair);
         btnSair.setOnClickListener(v -> {
-            Intent intent = new Intent(com.example.forca.MainActivity6.this, MainActivity.class);
+            Intent intent = new Intent(MainActivity6.this,MainActivity.class);
+
             startActivity(intent);
             finish();
         });
 
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        perna_esq = findViewById(R.id.perna_esq);
+
+        btnA.setOnClickListener(v -> respostaCerta(btnA));
+        btnB.setOnClickListener(v -> respostaErrada(btnB));
+        btnC.setOnClickListener(v -> respostaErrada(btnC));
+
     }
+
+    private void respostaCerta(Button botao)
+    {
+        botao.setBackgroundColor(Color.GREEN);
+        pontos++;
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity6.this,MainActivity7.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+    private void respostaErrada(Button botao) {
+        botao.setBackgroundColor(Color.RED);
+        perna_esq.setColorFilter(Color.RED);
+
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity6.this, MainActivity7.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+
 }
--- app/src/main/java/com/example/forca/MainActivity7.java
+++ app/src/main/java/com/example/forca/MainActivity7.java
@@ -1,6 +1,11 @@
 package com.example.forca;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,61 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity7 extends AppCompatActivity {
 
+    Button btnSair, btnA, btnB, btnC;
+    ImageView perna_dir;
+    int pontos=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main7);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        pontos = getIntent().getIntExtra("pontosAtuais",0);
+
+        btnSair = findViewById(R.id.btnSair);
+        btnSair.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity7.this,MainActivity.class);
+
+            startActivity(intent);
+            finish();
         });
+
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        perna_dir = findViewById(R.id.perna_dir);
+
+        btnA.setOnClickListener(v -> respostaCerta(btnA));
+        btnB.setOnClickListener(v -> respostaErrada(btnB));
+        btnC.setOnClickListener(v -> respostaErrada(btnC));
+
     }
+
+    private void respostaCerta(Button botao)
+    {
+        botao.setBackgroundColor(Color.GREEN);
+        pontos++;
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity7.this,MainActivity.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+    private void respostaErrada(Button botao) {
+        botao.setBackgroundColor(Color.RED);
+        perna_dir.setColorFilter(Color.RED);
+
+        new Handler().postDelayed(()->{
+            botao.setBackgroundColor(Color.TRANSPARENT);
+            Intent intent = new Intent(MainActivity7.this, MainActivity.class);
+            intent.putExtra("pontosAtuais",pontos);
+            startActivity(intent);
+            finish();
+        },500);
+    }
+
+
 }
