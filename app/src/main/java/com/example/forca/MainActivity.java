c4266656cd9f0573d5bc141e90122872fe659607
Vinicius Ferreira <41nicius.aleg@gmail.com>
2025-11-24 18:40:28 -0300

    telas de perguntas prontas

--- app/src/main/java/com/example/forca/MainActivity.java
+++ app/src/main/java/com/example/forca/MainActivity.java
@@ -1,27 +1,23 @@
 package com.example.forca;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);   // <-- FALTAVA ISSO!
 
         Button btniniciar = findViewById(R.id.btniniciar);
+
         btniniciar.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
             startActivity(intent);
         });
-
     }
 }
--- app/src/main/java/com/example/forca/MainActivity6.java
+++ app/src/main/java/com/example/forca/MainActivity6.java
@@ -13,16 +13,16 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.forca.MainActivity;
 import com.example.forca.R;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main2);
 
         Button btnSair = findViewById(R.id.btnSair);
         btnSair.setOnClickListener(v -> {
-            Intent intent = new Intent(com.example.forca.MainActivity3.this, MainActivity.class);
+            Intent intent = new Intent(com.example.forca.MainActivity6.this, MainActivity.class);
             startActivity(intent);
             finish();
         });
